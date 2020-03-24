3a392adbe038cbaa643d8f6d55d71d7afca10f12
Stefan Nožinić <stefan@lugons.org>
2020-03-24 19:24:04 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#docker run -it -p 8080:8080 -p 8001:8001 --name drools-workbench jboss/drools-workbench:latest
+docker run -it -p 8080:8080 -p 8001:8001 --name drools-workbench-showcase jboss/drools-workbench-showcase:latest
+
